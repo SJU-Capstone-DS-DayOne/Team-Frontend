f0d88c3119193bc34d75227cbfcb86448db05c86
류지승 <ryujiseung@lyujiseung-ui-MacBookPro.local>
2024-06-09 16:03:20 +0900

    1235

--- src/apis/getCoupleCode.js
+++ src/apis/getCoupleCode.js
@@ -13,9 +13,11 @@ export const getCoupleCode = async () => {
             const { code } = response.data;
 
             return code;
-        } else if (response.status == 403) console.log(1);
+        }
     } catch (error) {
         console.error(error);
+
+        if (error.request.status === 403) console.log(123);
         return false;
     }
 };
